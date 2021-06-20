fa4cc8365ea8b960c37a5acf3aabbb8b5200b1b7
Nick <merlyn022@gmail.com>
2021-06-20 18:57:06 +0300

    Create install_docker.sh

---
+++ install_docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+apt-get remove docker docker-engine docker.io containerd runc
+
+apt-get update
+
+apt-get install apt-transport-https ca-certificates curl gnupg lsb-release -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt sources.list.d/docker.list > /dev/null
+
+apt-get update
+
+apt-get install docker-ce docker-ce-cli containerd.io
